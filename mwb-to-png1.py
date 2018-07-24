73ac7f5abaed919391f336ec1120c2f9bdcd2bde
Guillaume Poncioni <gponcioni@kimoce.com>
2018-07-24 11:36:14 +0200

    et la ça marche?

--- mwb-to-png1.py
+++ mwb-to-png1.py
@@ -7,11 +7,11 @@ import os
 import shutil
 
 directory = sys.argv[1]
-needCommit = False
+# needCommit = False
 
 for element in os.listdir(directory):
     if (element.endswith('.mwb') and os.path.isfile(directory + element[0:-4] + '.png') == False):
-      needCommit = True
+      # needCommit = True
       shutil.copyfile(directory + element, 'temp.mwb')
       os.system('/usr/bin/mysql-workbench --run-script mwb-to-png2.py')
       with Image(filename = 'temp.pdf[0]', resolution = 300) as img:
@@ -20,5 +20,5 @@ for element in os.listdir(directory):
       os.remove('temp.pdf')
       os.system('git add ' + directory + element[0:-4] + '.png')
 
-if (needCommit):
-    os.system('git commit --amend')
+# if (needCommit):
+#     os.system('git commit --amend')
