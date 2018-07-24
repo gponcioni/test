36a3e63273ce74591a7d8b3360962306f1e810ed
Guillaume Poncioni <gponcioni@kimoce.com>
2018-07-24 11:02:52 +0200

    aller stp marche

--- mwb-to-png1.py
+++ mwb-to-png1.py
@@ -12,7 +12,7 @@ directory = sys.argv[1]
 for element in os.listdir(directory):
     if element.endswith('.mwb'):
     	shutil.copyfile(directory + element, 'temp.mwb')
-        os.system('/usr/bin/mysql-workbench --run-script retest2.py')
+        os.system('/usr/bin/mysql-workbench --run-script mwb-to-png2.py')
         with Image(filename = 'temp.pdf[0]', resolution = 300) as img:
             img.save(filename = directory + element[0:-4] + '.png')
         os.remove('temp.mwb')
